50247f28375de36516cb4c043e08611274e6bd6c
3ter <dritterseinistalles@gmail.com>
2020-09-12 13:09:15 +0200

    Add a new font and display it with arbitrary size
    Include necessary modules (truetype)

--- basic_pixel_boilerplate.go
+++ basic_pixel_boilerplate.go
@@ -4,13 +4,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/text"
+	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/font"
 )
+
+func loadTTF(path string, size float64) (font.Face, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	font, err := truetype.Parse(bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return truetype.NewFace(font, &truetype.Options{
+		Size:              size,
+		GlyphCacheEntries: 1,
+	}), nil
+}
 
 func run() {
 	cfg := pixelgl.WindowConfig{
@@ -22,20 +48,26 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+	win.SetSmooth(true) // remove potential artifacts
 
-	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	face, err := loadTTF("intuitive.ttf", 52)
+	if err != nil {
+		panic(err)
+	}
+
+	atlas := text.NewAtlas(face, text.ASCII)
+	txt := text.New(pixel.V(100, 500), atlas)
+
+	for i := 0; i < 6; i++ {
+		txt.Orig.X += 10
+		txt.Orig.Y += 9000
+		fmt.Fprintln(txt, "Orig:", txt.Orig, "Dot:", txt.Dot)
+		// After (!) each line written text.Dot takes the X coordinate of text.Orig
+	}
 
 	for !win.Closed() {
 		win.Clear(colornames.Black)
-		basicTxt := text.New(pixel.V(100, 500), basicAtlas)
-
-		for i := 0; i < 6; i++ {
-			basicTxt.Orig.X += 10
-			basicTxt.Orig.Y += 9000
-			fmt.Fprintln(basicTxt, "Orig:", basicTxt.Orig, "Dot:", basicTxt.Dot)
-			// After (!) each line written text.Dot takes the X coordinate of text.Orig
-		}
-		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 1))
+		txt.Draw(win, pixel.IM.Scaled(txt.Orig, 1))
 		win.Update()
 	}
 }
