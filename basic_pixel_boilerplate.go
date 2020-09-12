cbcdb7464c43b75b6389df3439a727a12adcb55c
3ter <dritterseinistalles@gmail.com>
2020-09-12 11:13:07 +0200

    Follow the instructions of the tutorial to learn
    Add go module initialization so go finds modules in go/pkg and
    not only in go/src.

--- basic_pixel_boilerplate.go
+++ basic_pixel_boilerplate.go
@@ -3,8 +3,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 )
 
 func run() {
@@ -18,7 +23,16 @@ func run() {
 		panic(err)
 	}
 
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	basicTxt := text.New(pixel.V(100, 500), basicAtlas)
+
+	fmt.Fprintln(basicTxt, "Hello, text!")
+	fmt.Fprintln(basicTxt, "I support multiple lines!")
+	fmt.Fprintf(basicTxt, "And I'm an %s, yay!", "io.Writer")
+
 	for !win.Closed() {
+		win.Clear(colornames.Black)
+		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 3))
 		win.Update()
 	}
 }
