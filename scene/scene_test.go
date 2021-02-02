6babe727912cee9c4064b4b5c4692972f1464728
3ter <dritterseinistalles@gmail.com>
2021-02-02 21:43:04 +0100

    Add more tests

--- scene/scene_test.go
+++ scene/scene_test.go
@@ -9,4 +9,15 @@ func TestLoadFilesToSceneMap(t *testing.T) {
 	if len(scenesMap) == 0 {
 		t.Errorf("scenesMap is empty")
 	}
+	for sceneName, sceneObj := range scenesMap {
+		if sceneName == `Void` {
+			continue
+		}
+		if len(sceneObj.mapConfigPath) == 0 {
+			t.Errorf("The scene '" + sceneName + "' has no mapConfigPath")
+		}
+		if len(sceneObj.script.filePath) == 0 {
+			t.Errorf("The scene '" + sceneName + "' has no filePath")
+		}
+	}
 }
