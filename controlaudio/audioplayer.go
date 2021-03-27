686a9328b95796c6dd48e8c6ea44a75561c99d1a
chen <chen.li@posteo.net>
2021-03-27 18:10:41 +0100

    Commit current state to prepare for merging current master into this branch

--- controlaudio/audioplayer.go
+++ controlaudio/audioplayer.go
@@ -1,10 +1,26 @@
 //The audio player should be able to open an audio file via file name and play multiple tracks/stems in sync
 package controlaudio
 
+import (
+	"path/filepath"
+
+	"github.com/3ter/iMagine/fileio"
+	"github.com/faiface/beep/effects"
+)
+
+//AudioPlayer handles all music played throughout the game
 type AudioPlayer struct {
-	trackPath string
+	trackDir string
+	trackMap map[string]*effects.Volume
 }
 
-func (a *AudioPlayer) loadTrack(path string) {
-	a.trackPath = path
+func (a *AudioPlayer) loadTrack(trackName string) {
+	var trackPath = filepath.Join(a.trackDir, trackName)
+	a.trackMap[trackName] = fileio.GetStreamer(trackPath)
+}
+
+func (a *AudioPlayer) initAudioPlayer(path string) {
+	a.trackDir = path
+	a.trackMap = make(map[string]*effects.Volume)
+
 }
